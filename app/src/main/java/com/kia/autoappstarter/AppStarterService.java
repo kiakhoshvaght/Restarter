9e69c79f43d18afed93e99a0e3f0e8fdd44461fa
kia khoshvaght <k.khoshvaght>
2019-05-13 16:49:59 +0430

    Stop service button added and app now starts with to-be-closed intent and Logs added.

--- app/src/main/java/com/kia/autoappstarter/AppStarterService.java
+++ app/src/main/java/com/kia/autoappstarter/AppStarterService.java
@@ -1,17 +1,32 @@
 package com.kia.autoappstarter;
 
+import android.app.ActivityManager;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
 
 public class AppStarterService extends Service {
 
-    Service thisService;
+    private static final String TAG = AppStarterService.class.getName();
+    static AppStarterService thisService;
     CountDownTimer countDownTimer;
+
+    static void stopService(Context context){
+        context.stopService(new Intent(context,AppStarterService.class));
+    }
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG,"FUNCTION : onCreate");
+        super.onCreate();
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -20,36 +35,43 @@ public class AppStarterService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
+        Log.i(TAG,"FUNCTION : onStartCommand");
+        thisService = this;
         runServiceAsForeground();
         setupCountDownTimer();
-        return super.onStartCommand(intent, flags, startId);
+        return START_NOT_STICKY;
     }
 
     private void setupCountDownTimer() {
+        Log.i(TAG,"FUNCTION : setupCountDownTimer");
         countDownTimer = new CountDownTimer(999999999,10000) {
             @Override
             public void onTick(long l) {
+                Log.i(TAG,"FUNCTION : onTick");
                 startTheApp();
+//                closeAppAfterTheTime();
             }
 
 
             @Override
             public void onFinish() {
+                Log.i(TAG,"FUNCTION : onFinish");
                 setupCountDownTimer();
             }
         }.start();
     }
 
     private void startTheApp() {
-        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.ironmen");
+        Log.i(TAG,"FUNCTION : startTheApp");
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("ir.sakoo");
+        launchIntent.putExtra("to-be-closed","true");
         if (launchIntent != null) {
             startActivity(launchIntent);
         }
     }
 
     private void runServiceAsForeground() {
-
+        Log.i(TAG,"FUNCTION : runServiceAsForeground");
         Intent notificationIntent = new Intent(this, MainActivity.class);
 
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
@@ -61,5 +83,12 @@ public class AppStarterService extends Service {
                 .setContentIntent(pendingIntent).build();
 
         startForeground(1337, notification);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG,"FUNCTION : onDestroy");
+        countDownTimer.cancel();
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/kia/autoappstarter/MainActivity.java
+++ app/src/main/java/com/kia/autoappstarter/MainActivity.java
@@ -2,21 +2,57 @@ package com.kia.autoappstarter;
 
 
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getName();
+    Button stopBtn;
+    private ServiceConnection serviceConnection;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG,"FUNCTION : onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        setOnClickListeners();
+        if(AppStarterService.thisService == null) {
+            startAppStarterService();
+        }
+    }
 
-        startAppStarterService();
+    private void setOnClickListeners() {
+        Log.i(TAG,"FUNCTION : setOnClickListeners");
+        stopBtn.setOnClickListener(view -> {
+            Log.i(TAG,"FUNCTION : setOnClickListeners => on stopBtn click");
+            stopService(new Intent(this,AppStarterService.class));
+        });
+    }
+
+    private void findViews() {
+        Log.i(TAG,"FUNCTION : findViews");
+        stopBtn = findViewById(R.id.stop_btn);
     }
 
     private void startAppStarterService() {
-        Intent intent = new Intent(this, AppStarterService.class);
-        startService(intent);
+        Log.i(TAG,"FUNCTION : startAppStarterService");
+        startService(new Intent(this, AppStarterService.class));
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(TAG,"FUNCTION : onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(TAG,"FUNCTION : onDestroy");
+        super.onDestroy();
     }
 }
